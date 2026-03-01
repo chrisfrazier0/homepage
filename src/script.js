a1cf04d34a789072640830a075dc83a809e0964d
Chris Frazier <chrisfrazier0@users.noreply.github.com>
2026-03-01 01:39:53 -0500

    initial commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+(function () {
+  let msg = "%c Howdy 👋";
+  let styles = `
+            display: inline-block;
+            padding: 12px 0;
+            font-size: 14px;
+            font-family: monospace;`;
+  console.log(msg, styles);
+})();
